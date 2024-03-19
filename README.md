783c3b7461dbd69fa5558ff7e2153ce55bb9ef6b
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2024-03-19 18:14:24 -0300

    add README

---
+++ README.md
@@ -0,0 +1,16 @@
+Project "Ada Comics" for the Frontend Development Bootcamp in ADA ITW
+
+Deployment:
+
+Description: Ada Comics uses the Marvel API, allows you to search for comics or characters and learn their details
+
+API: Marvel API 
+
+Technologies: HTML, Tailwind, JavaScript
+
+Aditionals: Responsive design, light and dark mode 
+
+Authors: Sofía De Alessandre
+
+March 2024
+
--- js/app.js
+++ js/app.js
@@ -1,73 +1,65 @@
 /* UTILITIES */
-const urlBase = `http://gateway.marvel.com/v1/public/`
-const ts = `ts=1`
-const publicKey = "&apikey=61f276d1838483b47e28b9120ccfc29e"
-const hash = "&hash=c56011d7230c43adadab65f4f70e2111"
-//?ts=1&apikey=61f276d1838483b47e28b9120ccfc29e&hash=c56011d7230c43adadab65f4f70e2111
-const $ = (selector) => document.querySelector(selector)
-const $$ = (selector) => document.querySelectorAll(selector)
+const urlBase = `http://gateway.marvel.com/v1/public/`;
+const ts = `ts=1`;
+const publicKey = "&apikey=61f276d1838483b47e28b9120ccfc29e";
+const hash = "&hash=c56011d7230c43adadab65f4f70e2111";
 
-const showElement = (selector) => $(selector).classList.remove("hidden")
-const hideElement = (selector) => $(selector).classList.add("hidden")
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
 
-let offset = 0
-let offsetCharacters = 0
+const showElement = (selector) => $(selector).classList.remove("hidden");
+const hideElement = (selector) => $(selector).classList.add("hidden");
+
+let offset = 0;
+let offsetCharacters = 0;
 //_________________________________CHARACTERS
-const getCharacters = async(name, searched, offsetCharacters) =>{
-    let existName = name? `&nameStartsWith=${name}` :""
-    let existOffsetCh = offsetCharacters? `&offset=${offsetCharacters}` : ""
-    let searchedExist = searched? `&orderBy=${searched}` :""
- const url = `${urlBase}characters?${ts}${publicKey}${hash}${existName}${searchedExist}${existOffsetCh}`
- const response = await fetch(url)
- const data = await response.json()
- console.log(data.data)
- return(data.data.results);
-}
-getCharacters()
+const getCharacters = async (name, searched, offsetCharacters) => {
+  let existName = name ? `&nameStartsWith=${name}` : "";
+  let existOffsetCh = offsetCharacters ? `&offset=${offsetCharacters}` : "";
+  let searchedExist = searched ? `&orderBy=${searched}` : "";
+  const url = `${urlBase}characters?${ts}${publicKey}${hash}${existName}${searchedExist}${existOffsetCh}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.data.results;
+};
+getCharacters();
 
-//_________________________________COMICS PRUEBA DE OFFSET PARA COMICS
-const getComics = async(title, searched, offset) =>{
-    let existTitle = title? `&titleStartsWith=${title}` :""
-    let existOffset = offset? `&offset=${offset}` : ""
-    let searchedExist = searched? `&orderBy=${searched}` :""
-    const url = `${urlBase}comics?${ts}${publicKey}${hash}${existTitle}${searchedExist}${existOffset}`
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data.data.results);
-    console.log(data.data)
-    return data.data.results
-   }
-getComics()
+//_________________________________COMICS
+const getComics = async (title, searched, offset) => {
+  let existTitle = title ? `&titleStartsWith=${title}` : "";
+  let existOffset = offset ? `&offset=${offset}` : "";
+  let searchedExist = searched ? `&orderBy=${searched}` : "";
+  const url = `${urlBase}comics?${ts}${publicKey}${hash}${existTitle}${searchedExist}${existOffset}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.data.results;
+};
+getComics();
 
+//_________________________________________________ /* RENDERS */
 
-//_________________________________________________
-/* RENDERS */ 
 //_______________________________________________RENDER COMICS
+const renderComic = async (data) => {
+  hideElement("#render-cards");
+  showElement("#render-cards-detail");
+  const arrComic = data[0].creators.items;
+  const dataComic = `${urlBase}comics/${data[0].id}/characters?offset=${offset}&limit=20&${ts}${publicKey}${hash}`;
+  const response = await fetch(dataComic);
+  const data2 = await response.json();
+  let resultsData = data2.data.results.length;
+  const arrCharacters = data2.data.results;
+  let charactersComic = "";
 
+  for (let arr of arrCharacters) {
+    charactersComic += `${arr.name}, `;
+  }
 
-const renderComic = async(data) => {
-    hideElement("#render-cards")
-    showElement("#render-cards-detail")
-    const arrComic = data[0].creators.items
-    const infoNecesariaComic = `${urlBase}comics/${data[0].id}/characters?offset=${offset}&limit=20&${ts}${publicKey}${hash}`
-    const response = await fetch(infoNecesariaComic)
-    const data2 = await response.json()
-    let resultsData = data2.data.results.length
-    const arrCharacters = data2.data.results
-    let charactersComic = ""
+  let comicsCreators = "";
+  for (let arr of arrComic) {
+    comicsCreators += `${arr.name}, `;
+  }
 
-    for (let arr of arrCharacters){
-        console.log(arr)
-        charactersComic += `${arr.name}, `
-    }
-
-
-    let comicsCreators = ""
-    for(let arr of arrComic){
-     comicsCreators += `${arr.name}, `
-    }
-    
-    $("#render-cards-detail").innerHTML = `
+  $("#render-cards-detail").innerHTML = `
     <div class="m-2 p-2 gap-y-4 md:flex justify-center gap-4 lg:md:flex justify-center gap-4 xl:md:flex justify-center gap-4">
         <div>
              <img src="${data[0].thumbnail.path}/portrait_xlarge.${data[0].thumbnail.extension}" alt="">
@@ -86,59 +78,50 @@ const renderComic = async(data) => {
         <img src="" alt="">
         <p class="text-neutral-500">${charactersComic}</p>
      </div>
-    </div>`
-}
+    </div>`;
+};
 
-
-const renderComics = async(title, searched, offset) => {
-    const comics = await getComics(title, searched, offset)
-    $("#render-cards").innerHTML = ""
-    for(let comic of comics){
-        $("#render-cards").innerHTML += `
+const renderComics = async (title, searched, offset) => {
+  const comics = await getComics(title, searched, offset);
+  $("#render-cards").innerHTML = "";
+  for (let comic of comics) {
+    $("#render-cards").innerHTML += `
         <div onclick="getComicDetail(${comic.id})" class="m-2 flex justify-center flex-col gap-y-2">
          <img class="rounded-md" src="${comic.thumbnail.path}/portrait_xlarge.${comic.thumbnail.extension}" alt="">
-         <p class="font-semibold text-white">${comic.title}</p>
+         <p class="font-bold text-neutral-500">${comic.title}</p>
         </div>
-        `
-    }
-}
-renderComics()
+        `;
+  }
+};
+renderComics();
 
+const getComicDetail = async (comicId) => {
+  const url = `${urlBase}comics/${comicId}?${ts}${publicKey}${hash}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  renderComic(data.data.results);
+  return data.data.results;
+};
 
-const getComicDetail = async(comicId) => {
-        const url = `${urlBase}comics/${comicId}?${ts}${publicKey}${hash}`
-        const response = await fetch(url)
-        const data = await response.json()
-        renderComic(data.data.results)
-        console.log(data.data.results)
-         return data.data.results
-       }
-       
+//____________________________________________RENDER CHARACTERS
 
-//___________________________________________RENDER CHARACTERS
+const renderCharacter = async (data) => {
+  hideElement("#render-cards");
+  hideElement("#render-cards-detail");
+  hideElement("#render-characters");
+  showElement("#render-characters-detail");
+  const dataCharacter = `${urlBase}characters/${data[0].id}/comics?offset=${offsetCharacters}&limit=20&${ts}${publicKey}${hash}`;
+  const response = await fetch(dataCharacter);
+  const data2 = await response.json();
+  let totalData2 = data2.data.results.length;
+  const arrComics = data2.data.results;
+  let comicCharacters = "";
 
+  for (let arr of arrComics) {
+    comicCharacters += `${arr.title}, `;
+  }
 
-const renderCharacter = async(data) => {
-    hideElement("#render-cards")
-    hideElement("#render-cards-detail")
-    hideElement("#render-characters")
-    showElement("#render-characters-detail")
-    const infoNecesariaCharacter = `${urlBase}characters/${data[0].id}/comics?offset=${offsetCharacters}&limit=20&${ts}${publicKey}${hash}`
-    const response = await fetch(infoNecesariaCharacter)
-    const data2 = await response.json()
-    console.log(data2.data.results)
-    let totalData2 = data2.data.results.length
-    console.log(totalData2)
-    const arrComics = data2.data.results
-    let comicCharacters = ""
-
-    for (let arr of arrComics){
-        console.log(arr)
-        comicCharacters += `${arr.title}, `
-    }
-
-
-    $("#render-characters-detail").innerHTML = `
+  $("#render-characters-detail").innerHTML = `
     <div class="m-2 p-2 gap-y-4 md:flex justify-center gap-4 lg:md:flex justify-center gap-4 xl:md:flex justify-center gap-4">
         <div>
              <img src="${data[0].thumbnail.path}/portrait_xlarge.${data[0].thumbnail.extension}" alt="">
@@ -155,152 +138,134 @@ const renderCharacter = async(data) => {
         <img src="" alt="">
         <p>${comicCharacters}</p>
      </div>
-    </div>`
-  
-}
+    </div>`;
+};
 
-const renderCharacters = async(name, searched, offsetCharacters) => {
-    const characters = await getCharacters(name, searched, offsetCharacters)
-    $("#render-characters").innerHTML = ""
-    for(let character of characters){
-        $("#render-characters").innerHTML += `
+const renderCharacters = async (name, searched, offsetCharacters) => {
+  const characters = await getCharacters(name, searched, offsetCharacters);
+  $("#render-characters").innerHTML = "";
+  for (let character of characters) {
+    $("#render-characters").innerHTML += `
         <div onclick="getCharacterDetail(${character.id})" class="m-2 flex justify-center flex-col gap-y-2">
          <img class="rounded-md" src="${character.thumbnail.path}/portrait_xlarge.${character.thumbnail.extension}" alt="">
-         <p class="font-semibold text-white">${character.name}</p>
+         <p class="font-bold text-neutal-500">${character.name}</p>
         </div>
-        `
-    }
-}
-renderCharacters()
+        `;
+  }
+};
+renderCharacters();
 
-const getCharacterDetail = async(characterId) => {
-    const url = `${urlBase}characters/${characterId}?${ts}${publicKey}${hash}`
-    const response = await fetch(url)
-    const data = await response.json()
-    renderCharacter(data.data.results)
-    return data.data.results
-   }
-
+const getCharacterDetail = async (characterId) => {
+  const url = `${urlBase}characters/${characterId}?${ts}${publicKey}${hash}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  renderCharacter(data.data.results);
+  return data.data.results;
+};
 
 /* EVENTS */
 
-$("#input-search").addEventListener("change", () => {
-    //renderComics($("#input-search").value)
-    //renderCharacters($("#input-search").value)
-})
-$("#select-type").addEventListener("onchange", () => {
-    if ($("#select-type").value == "characters"){
-        showElement("#render-characters")
-        hideElement("#render-cards")
-        hideElement("#render-cards-detail")
-    } else{
-        showElement("#render-cards")
-        hideElement("#render-characters")
-        hideElement("#render-characters-detail")
-    }
-})
-$("#select-order").addEventListener("onchange", () => {
-    if (("#select-order").value == "title"){
-    $("#change-value").value = "name"
-    console.log(("#select-order").value)
-} 
-     else if (("#select-order").value == "-title") {
-    let changeTitle2 = "-name"
-    ("#select-order").value = changeTitle2
+const initialize = () => {
+  $("#input-search").addEventListener("change", () => {});
+  $("#select-type").addEventListener("onchange", () => {
+    if ($("#select-type").value == "characters") {
+      showElement("#render-characters");
+      hideElement("#render-cards");
+      hideElement("#render-cards-detail");
     } else {
-   // renderComics($("#input-search").value, $("#select-order").value)
-    console.log($("#select-order").value)
-}
-//renderCharacters($("#input-search").value, $("#select-order").value)
-}
-)
-$("#btn").addEventListener("click", () => {
-    if ($("#select-type").value == "characters"){
-        showElement("#render-characters")
-        hideElement("#render-cards")
-        hideElement("#render-cards-detail")
-    renderCharacters($("#input-search").value, $("#select-order").value)
-}    else if ($("#select-type").value == "comics"){
-    showElement("#render-cards")
-    hideElement("#render-characters")
-    hideElement("#render-characters-detail")
-        renderComics($("#input-search").value, $("#select-order").value)
-
+      showElement("#render-cards");
+      hideElement("#render-characters");
+      hideElement("#render-characters-detail");
     }
-    
-})
-
-$("#init").addEventListener("click", () => {
-    if($("#select-type").value == "comics"){
-        offset = 0
-        renderComics($("#input-search").value, $("#select-order").value, offset)
-    } else if($("#select-type").value == "characters") {
-        offsetCharacters = 0
-        console.log(offsetCharacters)
-        renderCharacters($("#input-search").value, $("#select-order").value, offsetCharacters)
-        
+  });
+  $("#select-order").addEventListener("onchange", () => {});
+  $("#btn").addEventListener("click", () => {
+    if ($("#select-type").value == "characters") {
+      showElement("#render-characters");
+      hideElement("#render-cards");
+      hideElement("#render-cards-detail");
+      renderCharacters($("#input-search").value, $("#select-order").value);
+    } else if ($("#select-type").value == "comics") {
+      showElement("#render-cards");
+      hideElement("#render-characters");
+      hideElement("#render-characters-detail");
+      renderComics($("#input-search").value, $("#select-order").value);
     }
-})
+    hideElement("#render-cards-detail");
+    hideElement("#render-characters-detail");
+  });
 
-     $("#next").addEventListener("click", () => {
-    if($("#select-type").value == "comics" && offset<3000){
-        offset += 20
-        console.log(offset)
-        renderComics($("#input-search").value, $("#select-order").value, offset)
-    } else if($("#select-type").value == "characters" && offsetCharacters<780) {
-        offsetCharacters += 20
-        console.log(offsetCharacters)
-        renderCharacters($("#input-search").value, $("#select-order").value, offsetCharacters)
-        
+  $("#init").addEventListener("click", () => {
+    if ($("#select-type").value == "comics") {
+      offset = 0;
+      renderComics($("#input-search").value, $("#select-order").value, offset);
+    } else if ($("#select-type").value == "characters") {
+      offsetCharacters = 0;
+      renderCharacters(
+        $("#input-search").value,
+        $("#select-order").value,
+        offsetCharacters
+      );
     }
-     })
+  });
 
+  $("#next").addEventListener("click", () => {
+    if ($("#select-type").value == "comics" && offset < 3000) {
+      offset += 20;
+      renderComics($("#input-search").value, $("#select-order").value, offset);
+    } else if (
+      $("#select-type").value == "characters" &&
+      offsetCharacters < 780
+    ) {
+      offsetCharacters += 20;
+      renderCharacters(
+        $("#input-search").value,
+        $("#select-order").value,
+        offsetCharacters
+      );
+    }
+  });
 
-     $("#prev").addEventListener("click", () => {
-        if($("#select-type").value == "comics" && offset>0){
-            offset -= 20
-            console.log(offset)
-            renderComics($("#input-search").value, $("#select-order").value, offset)
-        } else if($("#select-type").value == "characters" && offsetCharacters>0) {
-            offsetCharacters -= 20
-            console.log(offsetCharacters)
-            renderCharacters($("#input-search").value, $("#select-order").value, offsetCharacters)
-            
-        }
-         })
-    
+  $("#prev").addEventListener("click", () => {
+    if ($("#select-type").value == "comics" && offset > 0) {
+      offset -= 20;
+      renderComics($("#input-search").value, $("#select-order").value, offset);
+    } else if (
+      $("#select-type").value == "characters" &&
+      offsetCharacters > 0
+    ) {
+      offsetCharacters -= 20;
+      renderCharacters(
+        $("#input-search").value,
+        $("#select-order").value,
+        offsetCharacters
+      );
+    }
+  });
 
-     $("#end").addEventListener("click", () => {
-        if($("#select-type").value == "comics"){
-            offset = 30000
-            console.log(offset)
-            renderComics($("#input-search").value, $("#select-order").value, offset)
-        }
-        else if($("#select-type").value == "characters"){
-offsetCharacters = 780
-renderCharacters($("#input-search").value, $("#select-order").value, offsetCharacters)
-        }
-       
-     })
-     $("#modo").addEventListener("click", () => {
-        $("#body").classList.toggle("bg-blue-50")
-        $("#body").classList.toggle("bg-blue-950")
-        if($("#body").classList.contains("bg-blue-950")){
-            $("#modo").innerText = "💡 Modo claro"
-           $("#modo").classList.add("text-neutral-500")
-        }else{
-            $("#modo").innerText = "🌙 Modo oscuro"
-            $("#modo").classList.remove("text-white")
-        }
-
-
-        // if($("#body").classList.contains("bg-blue-50")){
-        //     $("#body").classList.add("bg-blue-950")
-        // } else if($("#body").classList.contains("bg-blue-950")){
-        //     $("#body").classList.remove("bg-blue-950")
-        //     $("#body").classList.add("bg-blue-50")
-        // }
-            
-        }
-     )
-     
+  $("#end").addEventListener("click", () => {
+    if ($("#select-type").value == "comics") {
+      offset = 30000;
+      renderComics($("#input-search").value, $("#select-order").value, offset);
+    } else if ($("#select-type").value == "characters") {
+      offsetCharacters = 780;
+      renderCharacters(
+        $("#input-search").value,
+        $("#select-order").value,
+        offsetCharacters
+      );
+    }
+  });
+  $("#mode").addEventListener("click", () => {
+    $("#body").classList.toggle("bg-blue-50");
+    $("#body").classList.toggle("bg-blue-950");
+    if ($("#body").classList.contains("bg-blue-950")) {
+      $("#mode").innerText = "💡 Modo claro";
+      $("#mode").classList.add("text-neutral-500");
+    } else {
+      $("#mode").innerText = "🌙 Modo oscuro";
+      $("#mode").classList.remove("text-white");
+    }
+  });
+};
+window.addEventListener("load", initialize);
